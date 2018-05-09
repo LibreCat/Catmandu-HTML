3911944cf7b4deef95c9babc9af9b449effff1a9
Patrick Hochstenbach <patrick.hochstenbach@ugent.be>
2018-05-09 09:38:43 +0200

    Adding POD

--- lib/Catmandu/Exporter/HTML.pm
+++ lib/Catmandu/Exporter/HTML.pm
@@ -44,3 +44,44 @@ sub commit {
 }
 
 1;
+
+__END__
+
+=pod
+
+=head1 NAME
+
+Catmandu::Exporter::HTML - a HTML exporter
+
+=head1 SYNOPSIS
+
+    # From the commandline
+    $ catmandu convert HTML --fix myfixes to HTML < ex/test.html
+
+    # From Perl
+
+    use Catmandu;
+
+    # Print to STDOUT
+    my $exporter = Catmandu->exporter('HTML');
+
+    $exporter->add_many($arrayref);
+    $exporter->add_many($iterator);
+    $exporter->add_many(sub { });
+
+    $exporter->add($hashref);
+
+    printf "exported %d objects\n" , $exporter->count;
+
+    # Get an array ref of all records exported
+    my $data = $exporter->as_arrayref;
+
+=head1 DESCRIPTION
+
+This is a L<Catmandu::Exporter> for converting Perl into HTML.
+
+=head1 SEE ALSO
+
+L<Catmandu::Importer::LIDO>, L<HTML::TokeParser>
+
+=cut
--- lib/Catmandu/Importer/HTML.pm
+++ lib/Catmandu/Importer/HTML.pm
@@ -31,3 +31,66 @@ sub generator {
 }
 
 1;
+
+__END__
+
+=pod
+
+=head1 NAME
+
+Catmandu::Importer::HTML - An HTML importer
+
+=head1 SYNOPSIS
+
+    # From the command line
+    $ catmandu convert HTML to YAML < ex/test.html
+
+    # From Perl
+    use Catmandu;
+
+    my $importer = Catmandu->importer('HTML',file => 'ex/test.html');
+
+    my $n = $importer->each(sub {
+        my $hashref = $_[0];
+        # ...
+    });
+
+=head1 DESCRIPTION
+
+This is a L<Catmandu::Importer> for converting HTML data using the
+L<HTML::TokeParser> parser.
+
+=head1 CONFIGURATION
+
+=over
+
+=item file
+
+Read input from a local file given by its path. Alternatively a scalar
+reference can be passed to read from a string.
+
+=item fh
+
+Read input from an L<IO::Handle>. If not specified, L<Catmandu::Util::io> is used to
+create the input stream from the C<file> argument or by using STDIN.
+
+=item encoding
+
+Binmode of the input stream C<fh>. Set to C<:utf8> by default.
+
+=item fix
+
+An ARRAY of one or more fixes or file scripts to be applied to imported items.
+
+=back
+
+=head1 METHODS
+
+Every L<Catmandu::Importer> is a L<Catmandu::Iterable> all its methods are
+inherited.
+
+=head1 SEE ALSO
+
+L<Catmandu::Importer>, L<HTML::TokeParser>
+
+=cut
